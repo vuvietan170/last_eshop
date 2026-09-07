e28395f80d0406059d9e1784aa8861d4f67b2bc1
TanVu170 <vuvietan17102005@gmail.com>
2026-09-07 17:57:53 +0700

    app provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import '@/App.css'
-import { Button } from './components/ui/button'
-import Navbar from './components/Navbar'
+import "@/App.css";
+import { Button } from "./components/ui/button";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./hooks/useTheme";
 
 function App() {
-  return <>
-  <div className='min-h-screen bg-paper text-ink '>
-  <Navbar/>
-  </div>
-  </>
+    return (
+        <>
+            <ThemeProvider>
+                <div className="min-h-screen bg-paper text-ink ">
+                    <Navbar />
+                </div>
+            </ThemeProvider>
+        </>
+    );
 }
 
-export default App
+export default App;
