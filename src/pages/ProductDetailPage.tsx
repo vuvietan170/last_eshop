5ea4cf473bb9a09a1d4595397f010ecd6b1e0d66
TanVu170 <vuvietan17102005@gmail.com>
2026-09-09 15:38:50 +0700

    feat: ProductDetailpage

---
+++ src/pages/ProductDetailPage.tsx
@@ -0,0 +1,6 @@
+// nhận vào 1 prop id: number để cần biết chính xác sản phẩm nào vì mỗi sản phẩm chỉ có 1 di duy nhất
+const ProductDetailPage = ({ id }: { id: number }) => {
+    return <div className="p-8">Đây là trang chi tiết sản phẩm</div>;
+};
+
+export default ProductDetailPage;
