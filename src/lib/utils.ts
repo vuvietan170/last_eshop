3a14a36011298fcaecc1edbbe53f6898bd61726a
TanVu170 <vuvietan17102005@gmail.com>
2026-09-10 21:08:03 +0700

    feat:getDiscountedPrice

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,9 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
+}
+
+export function getDiscountedPrice(price: number, discountPercentage: number) {
+    return price * (1 - discountPercentage / 100);
 }
