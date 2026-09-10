f26323b0a30d8a4346bfc0fdae4e305fbe473c81
TanVu170 <vuvietan17102005@gmail.com>
2026-09-10 23:24:38 +0700

    feat: type ProductDetail

--- src/types/products.ts
+++ src/types/products.ts
@@ -2,11 +2,17 @@
 export type Product = {
     id: number;
     title: string;
-    description: string
-    images: string[]
-    thumbnail: string
-    price: number
-    discountPercentage: number
-    rating: number
-    category: string
-}
+    description: string;
+    images: string[];
+    thumbnail: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    category: string;
+};
+// Tách ra để lấy dữ liệu mà không cần đào sâu vào thằng Product
+export type ProductDetailPage = {
+    rating: number;
+    comment: string;
+    reviewerName: string;
+};
