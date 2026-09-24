62fce1de83880798a784c9c8f34b56d0dcc4a905
TanVu170 <vuvietan17102005@gmail.com>
2026-09-24 23:37:27 +0700

    feat: Authproider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Button } from "./components/ui/button";
 import { Navbar } from "./components/Navbar";
 import { matchRoute, useRouter } from "./router/router";
 import { ProductDetailPage } from "./pages/ProductDetailPage";
@@ -6,10 +5,26 @@ import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { SearchResultPage } from "./pages/SearchResultPage";
 import { OrderPage } from "./pages/OrderPage";
+import { LoginPage } from "./pages/LoginPage";
+import { useAuth } from "./hooks/useAuthContext";
+import { SellerPage } from "./pages/SellerPage";
 function App() {
-    const { path } = useRouter();
+    const { path, navigate } = useRouter();
+    const { user } = useAuth();
     const productMatch = matchRoute("/product/:id", path);
     const searchMatch = matchRoute("/search/:query", path);
+
+    // phân quyền
+    if (path === "/seller") {
+        if (!user) {
+            navigate("/login");
+            return null;
+        }
+        if (user.role !== "seller") {
+            return <p>Bạn không có quyền truy cập trang này</p>;
+        }
+        return <SellerPage />;
+    }
     return (
         <>
             <div className="min-h-screen bg-paper text-ink ">
@@ -21,6 +36,8 @@ function App() {
                         query={decodeURIComponent(searchMatch.query)}
                         // Hàm decode này dùng để giải mã ngược lại về chữ gốc khi nó bị mã hóa
                     />
+                ) : path === "/login" ? (
+                    <LoginPage />
                 ) : path === "/checkout" ? (
                     <CheckoutPage />
                 ) : path === "/orders" ? (
